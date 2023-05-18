7ac137c4878cb6d2b99626ba3ec516adde334327
Eubrenobrito <brenobrito_13@hotmail.com>
2023-05-18 20:57:45 -0300

    adc package.json

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
-class Cliente{
-    nome;
-    cpf;
-}
+import { Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 
-class ContaCorrente{
-    agencia;
-    // #saldo = 0;
-    _saldo = 0
-    sacar(valor){
-        if(this._saldo >= valor){
-        this._saldo -= valor;
-        }
-    }
-    depositar(valor){
-        if(valor > 0){
-            this._saldo5 += valor;
-        }
-    }
-}
 
 const cliente1 = new Cliente();
 
  cliente1.nome = 'Breno';
- cliente1.cpf = 022219552265;
+ cliente1.cpf = 1022219552265;
 
 const cliente2 = new Cliente();
  cliente2.nome = 'jurubeba';
- cliente2.cpf = 033319552265;
+ cliente2.cpf = 1033319552265;
 
 const contaCorrenteBreno = new ContaCorrente();
 
-contaCorrenteBreno.saldo = 0;
+contaCorrenteBreno._saldo = 0;
 contaCorrenteBreno.agencia = 1001;
 
 contaCorrenteBreno.depositar(100);
+contaCorrenteBreno.depositar(200);
+contaCorrenteBreno.depositar(200);
+contaCorrenteBreno.depositar(200);
+contaCorrenteBreno.depositar(200);
+
 contaCorrenteBreno.sacar(50);
 
 console.log(contaCorrenteBreno);
